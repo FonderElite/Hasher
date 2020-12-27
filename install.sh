8d2024c844971dfc4d61eba11e2ac23877198f47
FonderElite <71190959+FonderElite@users.noreply.github.com>
2020-12-28 06:06:04 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install python3
+sudo apt install python3-pip
+pip3 install -r requirements.txt
+echo"DONE"
+echo"
+|￣￣￣￣￣￣￣￣￣￣￣ |
+By: FonderElite
+|＿＿＿＿＿＿＿__________|
+(\__/) ||
+(•ㅅ•) ||
+/ 　 づ
+"
